c0bbe4e49eb7ff622ec58b67ae1492a412cdf51f
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-02-21 11:54:19 +0300

    validation schema with yup

--- src/features/auth/login/LoginPage.jsx
+++ src/features/auth/login/LoginPage.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import "./LoginPage.scss";
 import {InputText} from "primereact/inputtext";
-import {Formik, Form, Field} from "formik";
+import {Formik, Form, Field, ErrorMessage} from "formik";
 import {Button} from "primereact/button";
+import * as Yup from "yup";
+
 export default function LoginPage() {
 	//TODO: Style
 	//TODO: Formik implementation
@@ -14,10 +16,17 @@ export default function LoginPage() {
 	// validation schema => yup validation schema
 	// onSubmit
 	const initialUserCredentials = {email: "", password: ""};
+	const loginFormValidationSchema = Yup.object().shape({
+		email: Yup.string().required("Email girmek zorunludur."),
+		password: Yup.string()
+			.required("Şifre zorunludur.")
+			.min(3, "Şifre minimum 3 haneli olmalıdır."),
+	});
 	return (
 		<div className="container">
 			<Formik
 				initialValues={initialUserCredentials}
+				validationSchema={loginFormValidationSchema}
 				onSubmit={values => {
 					// kullanıcı butona bastı..
 					console.log(values);
@@ -28,7 +37,14 @@ export default function LoginPage() {
 						<div className="col-12 my-2">
 							<label>E-mail</label>
 							{/* <InputText name="email" placeholder="E-mail" /> */}
-							<Field className="form-control" name="email" />
+							<Field className="form-control" name="email" type="email" />
+							<div>
+								<ErrorMessage name="email">
+									{message => (
+										<div className="text-danger">HATA: {message}</div>
+									)}
+								</ErrorMessage>
+							</div>
 						</div>
 						<div className="col-12 my-2">
 							<label>Password</label>
@@ -38,6 +54,10 @@ export default function LoginPage() {
 								type="password"
 							/> */}
 							<Field className="form-control" name="password" type="password" />
+							<ErrorMessage
+								className="text-danger"
+								name="password"
+							></ErrorMessage>
 						</div>
 						<div className="col-12 my-2">
 							<Button type="submit" label="Login"></Button>

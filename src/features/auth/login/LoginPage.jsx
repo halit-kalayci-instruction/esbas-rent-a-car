ecac183dbc9ae06eaacb791fef7178a302300445
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-02-22 09:46:52 +0300

    login page html

--- src/features/auth/login/LoginPage.jsx
+++ src/features/auth/login/LoginPage.jsx
@@ -4,7 +4,8 @@ import {InputText} from "primereact/inputtext";
 import {Formik, Form, Field, ErrorMessage} from "formik";
 import {Button} from "primereact/button";
 import * as Yup from "yup";
-
+import {Card} from "primereact/card";
+import {Link} from "react-router-dom";
 export default function LoginPage() {
 	//TODO: Style
 	//TODO: Service implementation (HTTP REQUEST) axios
@@ -13,6 +14,7 @@ export default function LoginPage() {
 	// initial values => {email:'', password:''}
 	// validation schema => yup validation schema
 	// onSubmit
+	// NEXT.JS
 	const initialUserCredentials = {email: "", password: ""};
 	const loginFormValidationSchema = Yup.object().shape({
 		email: Yup.string().required("Email girmek zorunludur."),
@@ -20,49 +22,71 @@ export default function LoginPage() {
 			.required("Şifre zorunludur.")
 			.min(3, "Şifre minimum 3 haneli olmalıdır."),
 	});
+	//JSX => /klasor_adi/dosya_adi.png  => public/
 	return (
-		<div className="container">
-			<Formik
-				initialValues={initialUserCredentials}
-				validationSchema={loginFormValidationSchema}
-				onSubmit={values => {
-					// kullanıcı butona bastı..
-					console.log(values);
-				}}
-			>
-				<Form>
-					<div className="row my-5">
-						<div className="col-12 my-2">
-							<label>E-mail</label>
-							{/* <InputText name="email" placeholder="E-mail" /> */}
-							<Field className="form-control" name="email" type="email" />
-							<div>
-								<ErrorMessage name="email">
-									{message => (
-										<div className="text-danger">HATA: {message}</div>
-									)}
-								</ErrorMessage>
-							</div>
-						</div>
-						<div className="col-12 my-2">
-							<label>Password</label>
-							{/* <InputText
+		<div className="login-background">
+			<div className="row m-0 content justify-content-center align-items-center">
+				<div className="col-3">
+					<Card>
+						<h2 className="text-center">Giriş Yap</h2>
+						<Formik
+							initialValues={initialUserCredentials}
+							validationSchema={loginFormValidationSchema}
+							onSubmit={values => {
+								// kullanıcı butona bastı..
+								console.log(values);
+							}}
+						>
+							<Form>
+								<div className="row">
+									<div className="col-12 my-2">
+										<label>E-mail</label>
+										{/* <InputText name="email" placeholder="E-mail" /> */}
+										<Field className="form-control" name="email" type="email" />
+										<div>
+											<ErrorMessage name="email">
+												{message => (
+													<div className="text-danger">HATA: {message}</div>
+												)}
+											</ErrorMessage>
+										</div>
+									</div>
+									<div className="col-12 my-2">
+										<label>Password</label>
+										{/* <InputText
 								name="password"
 								placeholder="Password"
 								type="password"
 							/> */}
-							<Field className="form-control" name="password" type="password" />
-							<ErrorMessage
-								className="text-danger"
-								name="password"
-							></ErrorMessage>
+										<Field
+											className="form-control"
+											name="password"
+											type="password"
+										/>
+										<ErrorMessage
+											className="text-danger"
+											name="password"
+										></ErrorMessage>
+									</div>
+									<div className="col-12 my-2">
+										<Button
+											className="w-100"
+											type="submit"
+											label="Login"
+										></Button>
+									</div>
+								</div>
+							</Form>
+						</Formik>
+						<div className="w-100 text-center">
+							<p>
+								Don't have acoount?
+								<Link to={"/register"}> Sign Up</Link>
+							</p>
 						</div>
-						<div className="col-12 my-2">
-							<Button type="submit" label="Login"></Button>
-						</div>
-					</div>
-				</Form>
-			</Formik>
+					</Card>
+				</div>
+			</div>
 		</div>
 	);
 }

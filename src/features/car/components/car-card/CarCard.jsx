fcabc64da8bd43c88d26419f4d4ac9ac823c3b17
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-02-24 09:21:53 +0300

    car service implementation

--- src/features/car/components/car-card/CarCard.jsx
+++ src/features/car/components/car-card/CarCard.jsx
@@ -16,25 +16,24 @@ export default function CarCard(props) {
 		</div>
 	);
 
+	//TODO: Backend'e image ve özellikler ekle.
 	return (
 		<div className="card flex justify-content-center">
 			<Card footer={footer} header={header} className="md:w-25rem">
-				<h3>{props.brand}</h3>
-				<h5>{props.model}</h5>
-				<Divider />
-				<p className="m-0">{props.description}</p>
+				<h3>{props.car.brandName}</h3>
+				<h5>{props.car.modelName}</h5>
 				<Divider />
 				<div className="row">
 					<div className="col-3">
 						<div className="d-flex justify-content-center align-items-center flex-column">
 							<i className="pi pi-car"></i>
-							BMW x5
+							{props.car.plate}
 						</div>
 					</div>
 					<div className="col-3">
 						<div className="d-flex justify-content-center align-items-center flex-column">
 							<i className="pi pi-clock"></i>
-							2021
+							{props.car.modelYear}
 						</div>
 					</div>
 					<div className="col-3">
@@ -50,6 +49,7 @@ export default function CarCard(props) {
 						</div>
 					</div>
 				</div>
+				<Divider />
 			</Card>
 		</div>
 	);
--- src/shared/pages/homepage/Homepage.jsx
+++ src/shared/pages/homepage/Homepage.jsx
@@ -1,23 +1,27 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import CarCard from "../../../features/car/components/car-card/CarCard";
+import CarService from "../../../features/car/services/carService";
 
 export default function Homepage() {
 	//TODO: Pagination
+	const [cars, setCars] = useState([]);
+	useEffect(() => {
+		// Backendden araba bilgilerini getirme
+		let carService = new CarService();
+		carService.getAll().then(response => {
+			// Gelen cevabı ekrana yazdırma
+			console.log(response);
+			setCars(response.data.items);
+		});
+	}, []);
 	return (
 		<div className="container">
 			<div className="row mt-3">
-				<div className="col-12 mb-2 col-md-3">
-					<CarCard brand="BMW" model="520"></CarCard>
-				</div>
-				<div className="col-12 mb-2 col-md-3">
-					<CarCard brand="Fiat" model="Egea"></CarCard>
-				</div>
-				<div className="col-12 mb-2 col-md-3">
-					<CarCard brand="Ford" model="Focus"></CarCard>
-				</div>
-				<div className="col-12 mb-2 col-md-3">
-					<CarCard brand="Alfa Romeo" model="520"></CarCard>
-				</div>
+				{cars.map(car => (
+					<div className="col-12 mb-2 col-md-3">
+						<CarCard car={car}></CarCard>
+					</div>
+				))}
 			</div>
 		</div>
 	);

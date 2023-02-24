7ef91d6fab55859a903b48ebd7b33983972fdcf6
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-02-24 10:42:35 +0300

    pagination

--- src/shared/pages/homepage/Homepage.jsx
+++ src/shared/pages/homepage/Homepage.jsx
@@ -1,24 +1,31 @@
 import React, {useEffect, useState} from "react";
 import CarCard from "../../../features/car/components/car-card/CarCard";
 import CarService from "../../../features/car/services/carService";
+import "./Homepage.scss";
 
 export default function Homepage() {
 	//TODO: Pagination
 	// Redux
 	const [cars, setCars] = useState([]);
-	const [pagination, setPagination] = useState({index: 0});
+	const [pagination, setPagination] = useState({});
 	useEffect(() => {
 		// Backendden araba bilgilerini getirme
+		fetchCarData();
+	}, []);
+	// Circular Hook Call
+
+	//TODO: User select for page size
+	const fetchCarData = (page = 0) => {
 		let carService = new CarService();
-		carService.getAll(pagination.index, 1).then(response => {
-			// Gelen cevabı ekrana yazdırma
+		carService.getAll(page, 1).then(response => {
 			setCars(response.data.items);
 			setPagination(response.data);
-			console.log(pagination);
 		});
-	}, [pagination]);
+	};
 
-	const setPage = page => {};
+	const setPage = page => {
+		fetchCarData(page);
+	};
 
 	return (
 		<div className="container">
@@ -33,7 +40,14 @@ export default function Homepage() {
 				<ul className="pagination">
 					{pagination.hasPrevious && (
 						<li className="page-item">
-							<a className="page-link">Previous</a>
+							<a
+								onClick={() => {
+									setPage(pagination.index - 1);
+								}}
+								className="page-link"
+							>
+								Previous
+							</a>
 						</li>
 					)}
 					{[...Array(pagination.pages)].map((page, index) => (
@@ -42,12 +56,26 @@ export default function Homepage() {
 								"page-item " + (pagination.index == index ? "active" : "")
 							}
 						>
-							<a className="page-link">{index + 1}</a>
+							<a
+								onClick={() => {
+									setPage(index);
+								}}
+								className="page-link"
+							>
+								{index + 1}
+							</a>
 						</li>
 					))}
 					{pagination.hasNext && (
 						<li className="page-item">
-							<a className="page-link">Next</a>
+							<a
+								onClick={() => {
+									setPage(pagination.index + 1);
+								}}
+								className="page-link"
+							>
+								Next
+							</a>
 						</li>
 					)}
 				</ul>

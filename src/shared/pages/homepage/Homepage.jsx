76bff5a6950be1552227739e19f5dd024a220923
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-02-24 09:57:36 +0300

    pagination part 1

--- src/shared/pages/homepage/Homepage.jsx
+++ src/shared/pages/homepage/Homepage.jsx
@@ -4,16 +4,22 @@ import CarService from "../../../features/car/services/carService";
 
 export default function Homepage() {
 	//TODO: Pagination
+	// Redux
 	const [cars, setCars] = useState([]);
+	const [pagination, setPagination] = useState({index: 0});
 	useEffect(() => {
 		// Backendden araba bilgilerini getirme
 		let carService = new CarService();
-		carService.getAll().then(response => {
+		carService.getAll(pagination.index, 1).then(response => {
 			// Gelen cevabı ekrana yazdırma
-			console.log(response);
 			setCars(response.data.items);
+			setPagination(response.data);
+			console.log(pagination);
 		});
-	}, []);
+	}, [pagination]);
+
+	const setPage = page => {};
+
 	return (
 		<div className="container">
 			<div className="row mt-3">
@@ -23,6 +29,29 @@ export default function Homepage() {
 					</div>
 				))}
 			</div>
+			<nav aria-label="Page navigation example">
+				<ul className="pagination">
+					{pagination.hasPrevious && (
+						<li className="page-item">
+							<a className="page-link">Previous</a>
+						</li>
+					)}
+					{[...Array(pagination.pages)].map((page, index) => (
+						<li
+							className={
+								"page-item " + (pagination.index == index ? "active" : "")
+							}
+						>
+							<a className="page-link">{index + 1}</a>
+						</li>
+					))}
+					{pagination.hasNext && (
+						<li className="page-item">
+							<a className="page-link">Next</a>
+						</li>
+					)}
+				</ul>
+			</nav>
 		</div>
 	);
 }

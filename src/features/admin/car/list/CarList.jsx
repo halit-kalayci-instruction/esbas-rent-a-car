5eb0348db6400a50df5234e91ff192e3b839989a
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-03-07 11:01:52 +0300

    Car Delete Operation

--- src/features/admin/car/list/CarList.jsx
+++ src/features/admin/car/list/CarList.jsx
@@ -5,15 +5,21 @@ import {DataTable} from "primereact/datatable";
 import {Column} from "primereact/column";
 import CarService from "../../../car/services/carService";
 import Pagination from "../../../../shared/components/pagination/Pagination";
+import {Dialog} from "primereact/dialog";
+import toastr from "toastr";
+import {useNavigate} from "react-router-dom";
 function CarList() {
 	// Her bir satır için düzenle/sil butonlarının click aksiyonunu ata
 	// Listelemeye genel bir search ekle
 	// Tabloda gösterilen alanlar editable olsun.
 	// Silmek için modal oluştur.
 	const {t} = useTranslation();
+	const navigate = useNavigate();
 
 	const [data, setData] = useState([]);
 	const [pagination, setPagination] = useState({page: 0, pageSize: 1});
+	const [visible, setVisible] = useState(false);
+	const [carToDelete, setCarToDelete] = useState({});
 
 	useEffect(() => {
 		fetchCars();
@@ -34,7 +40,19 @@ function CarList() {
 	};
 
 	const deleteCar = car => {
-		console.log(car, " objesi silinmek isteniyor..");
+		setCarToDelete(car);
+		setVisible(true);
+	};
+
+	const confirmDelete = () => {
+		let carService = new CarService();
+		carService.delete(carToDelete.id).then(response => {
+			setVisible(false);
+			toastr.success("Araba başarıyla silindi.");
+			//fetchCars(); => Tekrar veritabanından bilgileri çek
+			// data = { hasPrevious:false, hasNext:true, index:0, pageSize:10, items:[] }
+			setData({...data, items: data.items.filter(i => i.id != carToDelete.id)}); // => Zaten silindi, memoryde silme işlemi gerçekleştir.
+		});
 	};
 
 	const buttonsTemplate = car => {
@@ -54,7 +72,10 @@ function CarList() {
 	return (
 		<div className="container mt-3">
 			<h3>{t("car.panel")}</h3>
-			<Button label={t("add.car")}></Button>
+			<Button
+				label={t("add.car")}
+				onClick={() => navigate("/car/add")}
+			></Button>
 			<div className="row mt-3">
 				<div className="col-12">
 					<DataTable value={data.items}>
@@ -76,6 +97,32 @@ function CarList() {
 					></Pagination>
 				</div>
 			</div>
+
+			<Dialog
+				header="DİKKAT!"
+				visible={visible}
+				style={{width: "50vw"}}
+				onHide={() => setVisible(false)}
+			>
+				<p className="m-0">
+					#{carToDelete.id} numaralı {carToDelete.plate} plaka nolu{" "}
+					{carToDelete.brandName} {carToDelete.modelName} aracını silmek
+					istediğinize emin misiniz?
+				</p>
+				<div className="mt-2 w-100 justify-content-end align-items-end text-end">
+					<Button
+						onClick={() => setVisible(false)}
+						label="İptal"
+						severity="secondary"
+					></Button>
+					<Button
+						onClick={() => confirmDelete()}
+						className="mx-3"
+						label="Sil"
+						severity="danger"
+					></Button>
+				</div>
+			</Dialog>
 		</div>
 	);
 }
--- src/features/car/services/carService.js
+++ src/features/car/services/carService.js
@@ -8,4 +8,8 @@ export default class CarService {
     add(car) {
         return instance.post("Cars", car);
     }
+
+    delete(id) {
+        return instance.delete("Cars/" + id);
+    }
 }

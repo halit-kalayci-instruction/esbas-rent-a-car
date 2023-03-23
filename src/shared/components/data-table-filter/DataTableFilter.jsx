9ed31b338da056f5d0647b26c645e791ed622473
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-03-23 09:18:51 +0300

    Filter Translator Refactor

--- src/shared/components/data-table-filter/DataTableFilter.jsx
+++ src/shared/components/data-table-filter/DataTableFilter.jsx
@@ -360,75 +360,77 @@ export default function DataTableFilter() {
 
 	//TODO: Chartlar
 	const translateFilterToBackend = filterObject => {
-		// Sadece filtreleme yapılan alanları getir.
-		let filtersToUse = Object.entries(filterObject.filters).filter(
+		let filtersAsObject = Object.entries(filterObject.filters).map(i => {
+			return {field: i[0], filter: i[1]};
+		});
+		let filtersToUse = filtersAsObject.filter(
 			i =>
-				(i[1].constraints &&
-					i[1].constraints.some(c => c.value && c.value != null)) ||
-				(i[1].value && i[1].value != null),
+				(i.filter.constraints &&
+					i.filter.constraints.some(c => c.value && c.value != null)) ||
+				(i.filter.value && i.filter.value != null),
 		);
+		let firstFilter = filtersToUse[0];
 
-		let parentFilter = {
-			field: filtersToUse[0][0],
-			operator: filtersToUse[0][1].constraints[0].matchMode,
-			value: filtersToUse[0][1].constraints[0].value,
-			logic: filtersToUse[0][1].operator,
-			filters: [],
-		};
+		let parentFilter = getFilterObject(firstFilter.field, firstFilter.filter);
 
 		filtersToUse.shift();
 
-		for (const [field, filter] of filtersToUse) {
-			if (field == "global") continue;
-			// constraints alanı var mı?
-			if (
-				filter.constraints &&
-				filter.constraints.some(
-					constraint => constraint.value && constraint.value != null,
-				) &&
-				filter.constraints.length > 0
-			) {
-				let obj = {
-					field: field,
-					logic: filter.operator,
-					operator: filter.constraints[0].matchMode,
-					value: filter.constraints[0].value,
-					filters: [],
-				};
-				filter.constraints.shift();
-				filter.constraints.forEach(constraint => {
-					obj.filters.push({
-						field: field,
-						logic: filter.operator,
-						operator: constraint.matchMode,
-						value: constraint.value,
-					});
-					parentFilter.filters.push(obj);
-				});
-			} else {
-				if (!filter.value || filter.value == null) continue;
-				let obj2 = {};
-				if (filter.matchMode == FilterMatchMode.BETWEEN) {
-					obj2 = {
-						field: field,
-						operator: "gt",
-						value: filter.value[0],
-						logic: "and",
-						filters: [{field: field, operator: "lt", value: filter.value[1]}],
-					};
-					console.log(obj2);
-				} else {
-					obj2 = {
-						field: field,
-						operator: filter.matchMode,
-						value: filter.value,
-					};
-					console.log(obj2);
-				}
-				parentFilter.filters.push(obj2);
-			}
+		for (const filter of filtersToUse) {
+			if (filter.field == "global") continue;
+			let obj = getFilterObject(filter.field, filter.filter);
+			parentFilter.filters.push(obj);
 		}
 		console.log(parentFilter);
+	};
+
+	// PrimeReact'ın anlayacağı filtre objesini Backendin anlayacağı objeye çevirmek..
+	const getFilterObject = (field, filter) => {
+		if (
+			filter.constraints &&
+			filter.constraints.some(
+				constraint => constraint.value && constraint.value != null,
+			) &&
+			filter.constraints.length > 0
+		) {
+			let obj = {
+				field: field,
+				logic: filter.operator,
+				operator: filter.constraints[0].matchMode,
+				value: filter.constraints[0].value,
+				filters: [],
+			};
+			filter.constraints.shift();
+			filter.constraints.forEach(constraint => {
+				obj.filters.push({
+					field: field,
+					logic: filter.operator,
+					operator: constraint.matchMode,
+					value: constraint.value,
+				});
+			});
+
+			return obj;
+		} else {
+			if (!filter.value || filter.value == null) return {};
+			let obj2 = {};
+			if (filter.matchMode == FilterMatchMode.BETWEEN) {
+				obj2 = {
+					field: field,
+					operator: "gt",
+					value: filter.value[0],
+					logic: "and",
+					filters: [{field: field, operator: "lt", value: filter.value[1]}],
+				};
+				return obj2;
+			} else {
+				obj2 = {
+					field: field,
+					operator: filter.matchMode,
+					value: filter.value,
+				};
+				return obj2;
+			}
+		}
 	};
 
 	return (

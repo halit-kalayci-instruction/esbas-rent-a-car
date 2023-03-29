0d69aa07ee41f1d9ffcf1b8d87ca6f706a5ef86f
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-03-29 11:34:35 +0300

    Refresh Token Giriş

--- src/core/utils/exceptionHandlers.js
+++ src/core/utils/exceptionHandlers.js
@@ -1,5 +1,7 @@
 import toastr from "toastr";
-
+import { getItem, setItem } from "./localStorage";
+import jwt_decode from "jwt-decode"
+import AuthService from "../../features/auth/services/authService";
 export const handleBusinessException = (error) => {
     toastr.error(error.Detail);
 }
@@ -9,7 +11,26 @@ export const handleValidationException = (error) => {
     })
 }
 export const handleAuthException = () => {
-    //TODO: Tokenin süresi geçti mi? Refresh etmeye çalış, edemez isek logout ol..
+    // Bu hatayı neden aldım?
+    // token var, süresi geçmemiş => rol yetersiz
+    let token = getItem("token");
+    let userInfo = jwt_decode(token);
+    let expired = Date.now() >= userInfo.exp * 1000;
+    if (!token || expired) {
+        // refresh-token
+        refreshToken();
+        return;
+    }
+    toastr.error("Bu işlemi yapmaya yetkiniz bulunmamaktadır.")
+}
+
+export const refreshToken = () => {
+    //TODO: coredan taşı
+    let authService = new AuthService();
+    authService.refreshToken().then(response => {
+        setItem("token", response.data.token);
+        //? Re-send request?
+    });
 }
 
 export const handleDefaultException = (error) => {
